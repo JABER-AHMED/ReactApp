7db3444a357ae202969f26e12f925e6c83aee587
JABER-AHMED <placidcse@gmail.com>
2024-01-17 23:29:08 +0100

    modified the eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,5 @@
 module.exports = {
-    "extends": [
-      "eslint:recommended",
-      "plugin:react/recommended"
-    ],
-    "plugins": ["react", "react-hooks"],
-    "parserOptions": {
-      "ecmaVersion": 2021,
-      "sourceType": "module",
-      "ecmaFeatures": {
-        "jsx": true
-      }
-    },
-    "globals": {
-      "test": "readonly",
-      "React": "readonly",
-      "expect": "readonly",
-      "document": "readonly"
-    },
-    "env": {
-      "node": true,
-      "es6": true
-    },
     "rules": {
-      "react/jsx-uses-react": "error",
-      "react/jsx-uses-vars": "error",
-      "react/jsx-indent": ["error", 2],
-      "react/jsx-props-no-spreading": "off",
-      "react-hooks/rules-of-hooks": "error",
-      "react-hooks/exhaustive-deps": "warn",
-      "react/prop-types": "error",
-      "react/jsx-boolean-value": ["error", "never"],
-      "react/jsx-closing-bracket-location": ["error", "tag-aligned"],
       "max-len": ["error", {
         "code": 80,
         "tabWidth": 2,
--- src/components/cart-list/card-list.component.jsx
+++ src/components/cart-list/card-list.component.jsx
@@ -2,16 +2,14 @@
 import "./card-list.styles.css";
 import Card from "../card/card.component";
 
-const CardList = ({monsters}) => {
-    return (
-        <div className="card-list">
-        {
-            monsters.map((monster) => (
-                <Card monster={monster} />
-            )) // this is the same as below
-        }
-        </div>
-    );
-}
+const CardList = ({ monsters }) => {
+  return (
+    <div className="card-list">
+      {monsters.map((monster) => (
+        <Card key={monster.id} monster={monster} />
+      ))}
+    </div>
+  );
+};
 
 export default CardList;
