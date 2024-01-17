b34809936eecaf23fd5283297127760798d1f908
JABER-AHMED <placidcse@gmail.com>
2024-01-17 03:46:19 +0100

    modified action

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,16 @@
 // .eslintrc.js
 module.exports = {
-    // parser: '@typescript-eslint/parser',
-    // plugins: ['@typescript-eslint'],
-    // extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
     rules: {
-      // Add specific rules as needed
-        "react/jsx-uses-react": "error",
-        "react/jsx-uses-vars": "error",
-        "react/jsx-boolean-value": ["error", "never"],
-        "react/jsx-closing-bracket-location": ["error", "tag-aligned"],
-        "react/jsx-indent": ["error", 2],
-        "react/jsx-props-no-spreading": "off",
-        "react/self-closing-comp": "error",
+        "max-len": ["error", {
+            "code": 80,
+            "tabWidth": 2,
+            "ignoreComments": true,
+            "ignoreTrailingComments": true,
+            "ignoreUrls": true,
+            "ignoreStrings": true,
+            "ignoreTemplateLiterals": true,
+            "ignoreRegExpLiterals": true
+        }]
     },
   };
   
